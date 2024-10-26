916e633feea0b87b74f1b3cf90786074b837b3b6
dhruv135 <dhruvgupta010@gmail.com>
2024-10-26 14:45:28 +0530

    commit message

--- src/main/java/com/dhruv/electronic/store/ElectronicStoreApplication.java
+++ src/main/java/com/dhruv/electronic/store/ElectronicStoreApplication.java
@@ -45,38 +45,38 @@ public class ElectronicStoreApplication implements CommandLineRunner {
 
         try {
 
-            Role role_admin = Role.builder().roleId(role_admin_id).roleName("ROLE_ADMIN").build();
-            Role role_normal = Role.builder().roleId(role_normal_id).roleName("ROLE_NORMAL").build();
+//            Role role_admin = Role.builder().roleId(role_admin_id).roleName("ROLE_ADMIN").build();
+//            Role role_normal = Role.builder().roleId(role_normal_id).roleName("ROLE_NORMAL").build();
+//
+//
+//            User adminUser = User.builder()
+//                    .name("admin")
+//                    .email("admin@gmail.com")
+//                    .password(passwordEncoder.encode("admin123"))
+//                    .gender("Male")
+//                    .imageName("default.png")
+//                    .roles(Set.of(role_admin, role_normal))
+//                    .userId(UUID.randomUUID().toString())
+//                    .about("I am admin User")
+//                    .build();
+//
+//            User normalUser = User.builder()
+//                    .name("durgesh")
+//                    .email("durgesh@gmail.com")
+//                    .password(passwordEncoder.encode("durgesh123"))
+//                    .gender("Male")
+//                    .imageName("default.png")
+//                    .roles(Set.of(role_normal))
+//                    .userId(UUID.randomUUID().toString())
+//                    .about("I am Normal User")
+//                    .build();
+//
+//            repository.save(role_admin);
+//            repository.save(role_normal);
 
 
-            User adminUser = User.builder()
-                    .name("admin")
-                    .email("admin@gmail.com")
-                    .password(passwordEncoder.encode("admin123"))
-                    .gender("Male")
-                    .imageName("default.png")
-                    .roles(Set.of(role_admin, role_normal))
-                    .userId(UUID.randomUUID().toString())
-                    .about("I am admin User")
-                    .build();
-
-            User normalUser = User.builder()
-                    .name("durgesh")
-                    .email("durgesh@gmail.com")
-                    .password(passwordEncoder.encode("durgesh123"))
-                    .gender("Male")
-                    .imageName("default.png")
-                    .roles(Set.of(role_normal))
-                    .userId(UUID.randomUUID().toString())
-                    .about("I am Normal User")
-                    .build();
-
-            repository.save(role_admin);
-            repository.save(role_normal);
-
-
-            userRepository.save(adminUser);
-            userRepository.save(normalUser);
+//            userRepository.save(adminUser);
+//            userRepository.save(normalUser);
 
         } catch (Exception e) {
             e.printStackTrace();
